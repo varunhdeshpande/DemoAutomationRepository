13d0e47f47f6b8aa2883ae5cbe385ab070d66b35
varun <varunhdeshpande@gmail.com>
2022-01-13 09:02:51 +0530

    FoorloopDiv3

---
+++ Web-Automation/src/com/sgtesting/tests/ForlloopDiv3.java
@@ -0,0 +1,20 @@
+package com.sgtesting.tests;
+
+public class ForlloopDiv3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,count=0;
+	    for(i=1;i<=100;i++)
+	    { 
+	     if(i%3==0)
+	     {
+	       count=count+1;
+	     }
+	}
+
+	       System.out.println(count);     
+
+	}
+
+}
